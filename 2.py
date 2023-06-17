fd52e9358cabc60080e5cb2af0027fe5b93bfff5
choorka29 <123003437+choorka29@users.noreply.github.com>
2023-06-17 07:17:21 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,31 @@
+import time
+
+def calc_hypotenuse(a, b):
+    c = (a**2 + b**2) ** 0.5
+    return c
+
+def log_calc_hypotenuse(func):
+    def wrapper(a, b):
+        start_time = time.time()
+        result = func(a, b)
+        end_time = time.time()
+
+        print(f"Время начала работы функции: {start_time}")
+        print(f"Стороны прямоугольного треугольника: a = {a}, b = {b}")
+        print(f"Результат вычисления: {result}")
+        print(f"Время окончания работы функции: {end_time}")
+        print(f"Время выполнения функции: {end_time - start_time} сек")
+
+        return result
+    return wrapper
+
+@log_calc_hypotenuse
+def calc_hypotenuse(a, b):
+    c = (a**2 + b**2) ** 0.5
+    return c
+
+a = float(input("Введите длину первого катета: "))
+b = float(input("Введите длину второго катета: "))
+
+hypotenuse = calc_hypotenuse(a, b)
+print(f"Длина гипотенузы: {hypotenuse}")
